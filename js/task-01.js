9005c782edf2f7000b44c07bc46bb6a227c9c34e
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-08 11:21:00 +0100

    Finished task 01

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const items = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach((item) => {
+  console.log(`Category: `, item.firstElementChild.textContent);
+  console.log(
+    `Elements: `,
+    item.firstElementChild.nextElementSibling.children.length
+  );
+});
