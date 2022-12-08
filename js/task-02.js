a5a66d6f88e6bba103e795a7ed7652a1bd314124
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-08 11:40:05 +0100

    Task 03 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("ul");
+
+ingredients.forEach((ingredient) => {
+  const newElem = document.createElement("li");
+  newElem.textContent = ingredient;
+  newElem.classList.add("item");
+  ingredientsList.append(newElem);
+});
