2c422b14e93548215825fcbaaa3c3e11dd9b126a
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-09 15:51:43 +0100

    Started task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+const inputFields = document.querySelectorAll("input");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (inputFields[0].value === "" || inputFields[1].value === "") {
+    alert("All fields must be filled in");
+  }
+});
+
+const {
+  email = loginForm.elements.email,
+  password = loginForm.elements.password,
+} = valuesObject;
+console.log(valuesObject);
