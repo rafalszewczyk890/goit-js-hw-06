c593aaebd4e00290ca2aaac347de9bea502de816
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-09 12:25:31 +0100

    Corrected task 06

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,10 @@
 const validationInput = document.querySelector("#validation-input");
 
 validationInput.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length >= 6) {
+  if (event.currentTarget.value.length >= validationInput.dataset.length) {
     validationInput.classList.replace("invalid", "valid");
+  } else if (event.currentTarget.value.length === 0) {
+    validationInput.classList.remove("valid", "invalid");
   } else {
     validationInput.classList.add("invalid");
   }
