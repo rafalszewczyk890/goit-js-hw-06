a9e7a4627f09122364feabd5481f5a2b667a21be
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-10 15:43:58 +0100

    Completed Task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeButton = document.querySelector("button");
+
+const documentBody = document.querySelector("body");
+
+const spanColor = document.querySelector(".color");
+
+changeButton.addEventListener("click", () => {
+  let randomColor = getRandomHexColor();
+  documentBody.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+  console.log(documentBody);
+});
