3e408c1f4586ec5e0893d471c1331e5929b3bb37
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-11 17:29:45 +0100

    Task 10 working

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controls = document.querySelector("#controls");
+
+const inputElem = document.querySelector("input");
+
+const boxesDiv = document.querySelector("#boxes");
+
+const createButton = document.querySelector("button[data-create]");
+
+const destroyButton = document.querySelector("button[data-destroy]");
+
+const generatedBox = `<div height="30" width="30" style="background-color: ${getRandomHexColor()}></div>`;
+
+createButton.addEventListener("click", (e) => {
+  const boxNumber = inputElem.value;
+  for (let i = 0; i < boxNumber; i += 1) {
+    const increase = 10 * i;
+    boxesDiv.insertAdjacentHTML(
+      "afterbegin",
+      `<div style="background-color: ${getRandomHexColor()}; height: ${
+        30 + increase
+      }px; width: ${30 + increase}px"></div>`
+    );
+  }
+});
