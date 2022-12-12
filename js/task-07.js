445394dbd4e9b4d80c1d92c1cb39456269af5bf7
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-12 10:51:20 +0100

    Corrected Task 07

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,9 @@ const sizeControl = document.querySelector("#font-size-control");
 
 const text = document.querySelector("#text");
 
+let sizeValue = `${sizeControl.value}px`;
+text.style.fontSize = sizeValue;
+
 sizeControl.addEventListener("input", () => {
   let sizeValue = `${sizeControl.value}px`;
   text.style.fontSize = sizeValue;
